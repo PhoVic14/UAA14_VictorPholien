b29350b4a10e64e84555d0892e4b98d9e49e166c
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-09-07 22:59:08 +0200

    Ajout exercice Chien

---
+++ PholienVictor_Chien/PholienVictor_Chien/Chien.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PholienVictor_Chien
+{
+    class Chien
+    {
+        private string _nom;
+        private string _race;
+        private int _age;
+        private bool _enOrdreDeVaccin;
+        private bool _puce;
+        private string _genre;
+        private bool _sterilise;
+
+        public UnChien(string nom, string race, int age, bool enOrdreDeVaccin, bool puce, string genre, bool sterilise, bool dresse)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+            _enOrdreDeVaccin = enOrdreDeVaccin;
+            _puce = puce;
+            _genre = genre;
+            _sterilise = sterilise;
+
+        }
+
+        public string AffichageChien()
+        {
+            string chaine = "Nom : " + _nom + "// Race : " + _race + "Age : " + _age + " // Vaccin : " + _enOrdreDeVaccin + " // Puce : " + _puce + " // Genre : " + _genre + " // Sterilisé : " + _sterilise + " // dresse : ";
+            return chaine;
+        }
+
+        public void Promener()
+        {
+            Console.WriteLine("Chien Promener");
+        }
+
+        public void Dresser()
+        {
+            Console.WriteLine("Chien Dresser");
+        }
+
+        public void Vacciner()
+        {
+            Console.WriteLine("Chien Vacciner");
+        }
+    }
+}
---
+++ PholienVictor_Chien/PholienVictor_Chien/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UAA14_Chien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(chien.AffichageChien());
+        }
+    }
+}
